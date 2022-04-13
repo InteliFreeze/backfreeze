b908b737e3b0c45aab827b50b4cc0086da680e6b
vSasakiv <vitorsakivenzelv@gmail.com>
2022-04-13 20:27:04 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 3000;
 const server = app.listen(port, (req, res) => {
   console.log(`App running on port ${port}...`);
   console.log("among us");
+  console.log("penis")
 
 });
 

72a37af75d20befa3cd47066555eba8e374ddd00
vSasakiv <vitorsakivenzelv@gmail.com>
2022-04-13 20:48:50 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
 const server = app.listen(port, (req, res) => {
   console.log(`App running on port ${port}...`);
-  
 });
 
+
+
 app.use((req, res, next) => {
-  res.send("hello")
+  res.send("hello");
   next();
 });

a12c003d282eda488e114a90bca7c2cd81c5f006
vSasakiv <63128590+vSasakiv@users.noreply.github.com>
2022-04-13 20:10:15 -0300

    server

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+  console.log("among us")
 });
 
 // fim da parada

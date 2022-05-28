4ce49caf64190f75b034fe496492fd49bb517186
Lucas Bertan <bertan1707@gmail.com>
2022-05-27 23:53:40 -0300

    Added cors

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ module.exports = app;
 
 //---------------------------------------------------------------------------------------------------------------//
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 app.use(express.json());
 
 app.use('/api/receitas', receitasRouter);
